771c1d3a434468a88d241ea39ac2739a27b6f902
Ali Can Gönüllü <alicangonullu@yahoo.com>
2022-09-11 17:28:52 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.alicangonullu.denemeprojesi;
+
+/**
+ *
+ * @author Ali Can Gönüllü
+ */
+
+public class DenemeProjesi {
+    public static void main(String[] args) {
+        System.out.println(PDUConverter.encode("+905369983658", "+905369983658", "Selamin Aleykum.").getPduCommand());
+    }
+ }
+
